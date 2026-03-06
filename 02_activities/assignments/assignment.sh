28a5fce29a5cb632947084f56c19f93c95bdfeaa
Emma Chen 2026 <ecfire745@hotmail.com>
2026-03-06 12:21:58 -0500

    6

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -42,6 +42,7 @@ mkdir ./data/processed
 cd ./data/processed 
 mkdir server_logs user_logs event_logs
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
+cd ../../
 cp ./data/raw/*server*.log ./data/processed/server_logs
 # 6. Repeat the above step for user logs and event logs
 cp ./data/raw/*user*.log ./data/processed/user_logs
